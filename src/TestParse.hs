f1a8bb3a0be9c1de1a962f895535f2c2e1dde4e7
cbpark <cbpark@gmail.com>
2014-06-03 15:48:06 +0200

    Add a test code to check parsing

---
+++ src/TestParse.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import           HEP.Data.LHEF.Parser            (parseEvent, stripLHEF)
+
+import           Control.Monad                   (when)
+import           Data.Attoparsec.ByteString.Lazy (Result (..), parse)
+import qualified Data.ByteString.Lazy.Char8      as C
+import           System.Environment              (getArgs)
+import           System.Exit
+
+main :: IO ()
+main = do
+  args <- getArgs
+
+  when (length args /= 1) $ do
+         putStrLn "Usage: testlhefparse filename"
+         exitFailure
+
+  C.readFile (head args) >>= parseAndPrint . stripLHEF
+
+parseAndPrint :: C.ByteString -> IO ()
+parseAndPrint str =
+    case parse parseEvent str of
+      Fail r _ _               -> C.putStr r
+      Done evRemained evParsed -> do
+                                print (snd evParsed)
+                                parseAndPrint evRemained
