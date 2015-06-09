bc07451581c2ed7e173438fd03f4a6f838bf163d
cbpark <cbpark@gmail.com>
2015-06-09 16:16:00 +0900

    Buildable with ghc 7.10, add shell.nix

---
+++ shell.nix
@@ -0,0 +1,22 @@
+with (import <nixpkgs> {}).pkgs;
+let pkg = haskellngPackages.callPackage
+            ({ mkDerivation, attoparsec, base, bytestring, conduit
+             , conduit-extra, containers, hep-kinematics, resourcet, stdenv
+             , transformers
+             }:
+             mkDerivation {
+               pname = "lhef-tools";
+               version = "0.1.0.0";
+               src = ./.;
+               isLibrary = true;
+               isExecutable = true;
+               buildDepends = [
+                 attoparsec base bytestring conduit conduit-extra containers
+                 hep-kinematics resourcet transformers
+               ];
+               homepage = "http://github.com/cbpark/lhef-tools";
+               description = "Tools for the LHEF analyses";
+               license = stdenv.lib.licenses.bsd3;
+             }) {};
+in
+  pkg.env
